a63e4c5ecdf1882e8544ae608863ff3a205fd8df
ABlazquezM <andrea.blazquez.martin@gmail.com>
2023-10-11 20:23:03 +0200

    Primer commit

---
+++ src/main/java/cesur/spring/andrea/TareaApiValidatoApplication.java
@@ -0,0 +1,13 @@
+package cesur.spring.andrea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaApiValidatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaApiValidatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cesur/spring/andrea/TareaApiValidatoApplicationTests.java
@@ -0,0 +1,13 @@
+package cesur.spring.andrea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaApiValidatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
